72442137cde030ebef654709b45398335bba1516
Thien Ngo <thiendngo@outlook.com>
2025-07-15 23:39:03 -0600

    Update Readme and remove unecessary files

--- src/app/services/order.spec.ts
+++ src/app/services/order.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { Order } from './order';
+import { beforeEach, describe, it } from 'node:test';
 
 describe('Order', () => {
   let service: Order;
@@ -11,6 +12,7 @@ describe('Order', () => {
   });
 
   it('should be created', () => {
+    // @ts-ignore
     expect(service).toBeTruthy();
   });
 });
